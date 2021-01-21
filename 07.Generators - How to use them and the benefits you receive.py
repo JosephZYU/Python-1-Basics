dcc54ba2326d564509197406bde305d7ea5c11f2
JosephYu <josephyu.careers@outlook.com>
2021-01-21 18:05:01 +0800

    Jan 21 PM

--- 07.Generators - How to use them and the benefits you receive.py
+++ 07.Generators - How to use them and the benefits you receive.py
@@ -6,17 +6,28 @@ it yields one result at a time 🌟
 so really this is waiting for us to ask for the
 next result so hasn't actually computed anything yet now
 
+😎 In most cases, we will use return () for coding interview~
+
+🧠 return: sends a specified value back to its caller
+    
+🧠 yield: can produce a sequence of values.
+
+We should use yield when we want to iterate over a sequence 
+but don’t want to store the entire sequence in memory.
+
+Ref - https://dzone.com/articles/when-to-use-yield-instead-of-return-in-python#:~:text=return%20sends%20a%20specified%20value,is%20used%20in%20Python%20generators.
+
 """
 
 # ✅ how to for loop with list comprehension? - YES - [i * i for i in nums]
-# 🎯✅ what's the yield vs. return in Python? - return (single item) yield (all items)
+# ✅ what's the yield vs. return in Python? - return (single item) yield (all items)
 
 
 def square_num(nums):
     # for num in nums:
     #     yield num * num
-    yield [i * i for i in nums]  # 200
-    # return [i * i for i in nums]  # 200
+    # yield [i * i for i in nums]  # 200
+    return [i * i for i in nums]  # 200
 
 
 new_nums = square_num([11, 12, 13])
